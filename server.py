12a7a25152f19b736bca0d0a8fa721a5d8243bb0
ndtrong <nguyenductrong12a1@gmail.com>
2024-05-19 10:45:48 +0700

    First commit

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/favicon.ico")
+def favicon():
+    return ""
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/<template_filename>")
+def template(template_filename):
+    return render_template(template_filename)
+
+app.run(debug=True)
